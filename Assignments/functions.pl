d7a4bd93c3dc07672b80e4a57edad6cb2f5c874a
Aleks Itskovich <31596660+ai223@users.noreply.github.com>
2021-04-19 22:54:03 -0400

    adding union function

--- Assignments/functions.pl
+++ Assignments/functions.pl
@@ -17,7 +17,15 @@ max(A, B, R) :- A < B, R is B.
 -- Q3 (Write a program that returns a list containing the union of the elements of two given lists.
 -- Assume the list represents sets (i.e. unique elements) and the union returns unique elements.)
 
-# TODO
+union([],[],[]).
+union(L1,[],L1).
+union([X|XS], L2, R) :-
+    inlist(X, L2), union(L2,XS,R).
+union([X|XS], L2, [X|R]):-
+    \+(inlist(X,L2)), union(L2,XS,R).
+
+inlist(X, [X|_]).
+inlist(X, [_|XS]) :- inlist(X, XS).
 
 -- Q4 (Write a program that returns the final element of a list)
 
